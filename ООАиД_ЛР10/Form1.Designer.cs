1dd763d3cfb3ff19050def7b706cbbe656006a8b
A5101 <saha.mars2012@gmail.com>
2022-03-03 18:24:28 +0300

    1

--- ООАиД_ЛР10/Form1.Designer.cs
+++ ООАиД_ЛР10/Form1.Designer.cs
@@ -43,7 +43,7 @@ namespace ООАиД_ЛР10
             this.StartMarketButton.Name = "StartMarketButton";
             this.StartMarketButton.Size = new System.Drawing.Size(94, 46);
             this.StartMarketButton.TabIndex = 0;
-            this.StartMarketButton.Text = "Начать";
+            this.StartMarketButton.Text = "Начать работу";
             this.StartMarketButton.UseVisualStyleBackColor = true;
             this.StartMarketButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
@@ -53,7 +53,7 @@ namespace ООАиД_ЛР10
             this.ChangeButton.Name = "ChangeButton";
             this.ChangeButton.Size = new System.Drawing.Size(94, 46);
             this.ChangeButton.TabIndex = 1;
-            this.ChangeButton.Text = "Изменить";
+            this.ChangeButton.Text = "Изменить состояние";
             this.ChangeButton.UseVisualStyleBackColor = true;
             this.ChangeButton.Click += new System.EventHandler(this.ChangeButton_Click);
             // 
@@ -62,50 +62,50 @@ namespace ООАиД_ЛР10
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(149, 8);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(181, 21);
+            this.checkBox1.Size = new System.Drawing.Size(273, 21);
             this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "Количество активаций";
+            this.checkBox1.Text = "Наблюдатель количества активаций";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(355, 8);
+            this.checkBox2.Location = new System.Drawing.Point(439, 9);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(161, 21);
+            this.checkBox2.Size = new System.Drawing.Size(258, 21);
             this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "Текущее состояние";
+            this.checkBox2.Text = "Наблюдатель текущего состояния";
             this.checkBox2.UseVisualStyleBackColor = true;
             this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // CurrStateTextBox
             // 
-            this.CurrStateTextBox.Location = new System.Drawing.Point(355, 36);
+            this.CurrStateTextBox.Location = new System.Drawing.Point(439, 36);
             this.CurrStateTextBox.Name = "CurrStateTextBox";
-            this.CurrStateTextBox.Size = new System.Drawing.Size(180, 22);
+            this.CurrStateTextBox.Size = new System.Drawing.Size(258, 22);
             this.CurrStateTextBox.TabIndex = 5;
             // 
             // CountActTextBox
             // 
             this.CountActTextBox.Location = new System.Drawing.Point(149, 36);
             this.CountActTextBox.Name = "CountActTextBox";
-            this.CountActTextBox.Size = new System.Drawing.Size(181, 22);
+            this.CountActTextBox.Size = new System.Drawing.Size(273, 22);
             this.CountActTextBox.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(549, 141);
+            this.ClientSize = new System.Drawing.Size(709, 141);
             this.Controls.Add(this.CountActTextBox);
             this.Controls.Add(this.CurrStateTextBox);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.ChangeButton);
             this.Controls.Add(this.StartMarketButton);
-            this.MaximumSize = new System.Drawing.Size(567, 188);
-            this.MinimumSize = new System.Drawing.Size(567, 188);
+            this.MaximumSize = new System.Drawing.Size(727, 188);
+            this.MinimumSize = new System.Drawing.Size(727, 188);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
